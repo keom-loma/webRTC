31c09e6bf07117ee6f70a839fc3d505041aaf0fd
keom-loma <keom.heang@lomatechnology.com>
2025-01-08 18:33:41 +0700

    add library for streaming video with webRTC

--- app/src/main/java/com/example/webrtc/MainActivity.kt
+++ app/src/main/java/com/example/webrtc/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.webrtc.ui.theme.WebRTCTheme
+import com.example.webrtc.ui.theme.WebRTCUi
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,9 +21,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             WebRTCTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    WebRTCUi(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
                     )
                 }
             }
